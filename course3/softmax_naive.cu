35c51988ab153170caee9a87078e2d07210de063
ypp977 <1808419119@qq.com>
2025-10-12 01:17:02 +0800

    course3 softmax 4 fix bug and add main

--- course3/softmax_naive.cu
+++ course3/softmax_naive.cu
@@ -204,98 +204,86 @@ __global__ void softmax_forward_kernel3(float* out, const float* inp, int N, int
 
 __global__ void softmax_forward_kernel4(float* out, const float* inp, int N, int C)
 {
-    // out is (N, C) just like inp. Each row of inp will get softmaxed.
-    // same as kernel3, but can handle any block size (multiple of 32)
-    // each row of C elements is handled by block_size threads
-    // furthermore, each block_size threads get executed in warps of 32 threads
-
-    // special reduction operations warpReduceMax/warpReduceSum are used for
-    // intra-warp reductions shared memory is used for inter-warp reduction
     extern __shared__ float shared[];
+    // 当前Block对应的索引
     int idx = blockIdx.x;
+    // 当前线程在Block内的索引
     int tid = threadIdx.x;
-    int warpId = threadIdx.x / 32; // warp index within a block
-    int laneId = threadIdx.x % 32; // thread index within a warp
-
-    // the number of warps per block. recall that blockDim.x is block_size
+    // 当前线程在当前Block内所属的warp索引
+    int warpId = tid / 32;
+    // 当前线程所在warp内的索引
+    int laneId = tid % 32;
+    // 每个Block内warp数量
     int warpsPerBlock = blockDim.x / 32;
 
-    // shared[] must be allocated to have 2 * warpsPerBlock elements
-    // first half for max values, the second half for sum values
     float* maxvals = shared;
     float* sumvals = &shared[warpsPerBlock];
 
-    // one row of inp, i.e. inp[idx, :] of shape (C,)
+    // 指向当前行的输入数据
     const float* x = inp + idx * C;
 
-    // first, thread coarsening by directly accessing global memory in series
+    // 分别计算每个线程局部最大值
     float maxval = -INFINITY;
     for (int i = tid; i < C; i += blockDim.x)
     {
         maxval = fmaxf(maxval, x[i]);
     }
-    // now within-warp reductions for maxval
+    // 归约计算每个warp内的最大值
     warpReduceMax(maxval);
-
-    // the 0th thread of each warp writes the maxval of that warp to shared memory
+    // 每个warp的最大值写入shared mem
     if (laneId == 0)
+    {
         maxvals[warpId] = maxval;
+    }
+
     __syncthreads();
 
-    // now the 0th thread reduces the maxvals in shared memory, i.e. across warps
+    // warp 内归约得到整行最大值
     if (tid == 0)
     {
-        float val = maxvals[tid];
+        float val = maxvals[0];
         for (int i = 1; i < warpsPerBlock; i++)
         {
             val = fmaxf(val, maxvals[i]);
         }
-        // store the final max in the first position
         maxvals[0] = val;
     }
     __syncthreads();
-    // broadcast the max to all threads
+
     float offset = maxvals[0];
 
-    // compute expf and write the result to global memory
     for (int i = tid; i < C; i += blockDim.x)
     {
         out[idx * C + i] = expf(x[i] - offset);
     }
 
-    // okay now we calculated exp(x - max(x))
-    // step 2: sum all the values and divide by the sum
-
-    // thread coarsening for sum
     x = out + idx * C;
-    float sumval = 0.0f;
+    float sumval = 0.f;
     for (int i = tid; i < C; i += blockDim.x)
     {
         sumval += x[i];
     }
-    // within-warp reduction for sumval
+
     warpReduceSum(sumval);
 
-    // write sumval to shared memory
     if (laneId == 0)
+    {
         sumvals[warpId] = sumval;
+    }
     __syncthreads();
 
-    // inter-thread reduction of sum
     if (tid == 0)
     {
-        float val = sumvals[tid];
-        for (int i = 1; i < warpsPerBlock; ++i)
+        float val = sumvals[0];
+        for (int i = 1; i < warpsPerBlock; i++)
         {
             val += sumvals[i];
         }
         sumvals[0] = val;
     }
     __syncthreads();
-    // broadcast the sum to all threads
     float sum = sumvals[0];
 
-    // divide the whole row by the sum
     for (int i = tid; i < C; i += blockDim.x)
     {
         out[idx * C + i] = x[i] / sum;
@@ -422,7 +410,38 @@ int main()
         cudaEventDestroy(start);
         cudaEventDestroy(stop);
     }
+    // GPU 版本4 ：使用warp + shared mem混合优化
+    {
+        cudaMemset(device_out, 0, num_elements * sizeof(float));
+        memset(out_gpu, 0, num_elements * sizeof(float));
 
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+
+        int block_size = 128;
+        int block_num = N;
+        int warpsPerBlock = block_size / 32;
+        size_t sharedmem_size = 2 * warpsPerBlock * sizeof(float);
+
+        cudaEventRecord(start);
+
+        softmax_forward_kernel4<<<block_num, block_size, sharedmem_size>>>(device_out, device_inp,
+                                                                           N, C);
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        float gpu_time_4 = 0;
+        cudaEventElapsedTime(&gpu_time_4, start, stop);
+
+        cudaMemcpy(out_gpu, device_out, num_elements * sizeof(float), cudaMemcpyDeviceToHost);
+
+        bool ok = compare_results(out_gpu, out_cpu, N, C);
+        printf("[kernel4] Results match: %s,\n[kernel4] GPU time: %.3fms\n", (ok ? "YES" : "NO"),
+               gpu_time_4);
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
     cudaFree(device_inp);
     cudaFree(device_out);
     free(inp);
